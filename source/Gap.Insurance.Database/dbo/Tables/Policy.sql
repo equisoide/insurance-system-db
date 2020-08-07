6fe58f269d5bb542d85046f2245f1fa33b5ce301
Juan Cuartas <equisoide@gmail.com>
2020-08-07 13:14:19 -0500

    Created Policy table

---
+++ source/Gap.Insurance.Database/dbo/Tables/Policy.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Policy]
+(
+	[PolicyId] INT NOT NULL,
+    [ClientId] INT NOT NULL,
+    [RiskId] INT NOT NULL,
+    [StatusId] INT NOT NULL,
+    [Name] NVARCHAR(50) NOT NULL,
+    [Description] NVARCHAR(250) NOT NULL,
+    [StartDate] DATETIME NOT NULL,
+    [Periods] INT NOT NULL,
+    [Price] DECIMAL(18,4) NOT NULL,
+	CONSTRAINT [PK_Policy] PRIMARY KEY CLUSTERED ([PolicyId] ASC)
+);
