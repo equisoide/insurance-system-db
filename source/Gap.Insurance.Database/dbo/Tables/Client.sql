fc321280b59d8fb4d8f46df36b9bde74a9f4dd91
Juan Cuartas <equisoide@gmail.com>
2020-08-07 14:14:45 -0500

    Adjusted indentation in sql scripts

--- source/Gap.Insurance.Database/dbo/Tables/Client.sql
+++ source/Gap.Insurance.Database/dbo/Tables/Client.sql
@@ -2,11 +2,11 @@
 (
 	[ClientId] INT IDENTITY(1,1) NOT NULL,
 	[Document] NVARCHAR(50) NOT NULL,
-    [Name] NVARCHAR(50) NOT NULL,
-    [Email] NVARCHAR(250) NOT NULL,
-    [CellPhone] NVARCHAR(250) NOT NULL,
-    [BirthDate] DATETIME NOT NULL,
+	[Name] NVARCHAR(50) NOT NULL,
+	[Email] NVARCHAR(250) NOT NULL,
+	[CellPhone] NVARCHAR(250) NOT NULL,
+	[BirthDate] DATETIME NOT NULL,
 
 	CONSTRAINT [PK_Client] PRIMARY KEY CLUSTERED ([ClientId] ASC),
-    CONSTRAINT [UK_Client_Document] UNIQUE ([Document] ASC)
+	CONSTRAINT [UK_Client_Document] UNIQUE ([Document] ASC)
 );
--- source/Gap.Insurance.Database/dbo/Tables/PolicyCoverage.sql
+++ source/Gap.Insurance.Database/dbo/Tables/PolicyCoverage.sql
@@ -1,13 +1,13 @@
 ﻿CREATE TABLE [dbo].[PolicyCoverage]
 (
 	[PolicyCoverageId] INT IDENTITY(1,1) NOT NULL,
-    [PolicyId] INT NOT NULL,
-    [CoverageId] INT NOT NULL,
-    [Percentage] DECIMAL(18,4) NOT NULL,
+	[PolicyId] INT NOT NULL,
+	[CoverageId] INT NOT NULL,
+	[Percentage] DECIMAL(18,4) NOT NULL,
 
 	CONSTRAINT [PK_PolicyCoverage] PRIMARY KEY CLUSTERED ([PolicyCoverageId] ASC),
-    CONSTRAINT [FK_PolicyCoverage_Coverage] FOREIGN KEY([CoverageId]) REFERENCES [dbo].[Coverage] ([CoverageId]),
-    CONSTRAINT [FK_PolicyCoverage_Policy] FOREIGN KEY([PolicyId]) REFERENCES [dbo].[Policy] ([PolicyId]),
-    CONSTRAINT [UK_PolicyCoverage] UNIQUE ([PolicyId] ASC, [CoverageId] ASC),
-    CONSTRAINT [CK_PolicyCoverage_Percentage] CHECK  ([Percentage] > 0 AND [Percentage] <= 100)
+	CONSTRAINT [FK_PolicyCoverage_Coverage] FOREIGN KEY([CoverageId]) REFERENCES [dbo].[Coverage] ([CoverageId]),
+	CONSTRAINT [FK_PolicyCoverage_Policy] FOREIGN KEY([PolicyId]) REFERENCES [dbo].[Policy] ([PolicyId]),
+	CONSTRAINT [UK_PolicyCoverage] UNIQUE ([PolicyId] ASC, [CoverageId] ASC),
+	CONSTRAINT [CK_PolicyCoverage_Percentage] CHECK  ([Percentage] > 0 AND [Percentage] <= 100)
 );
