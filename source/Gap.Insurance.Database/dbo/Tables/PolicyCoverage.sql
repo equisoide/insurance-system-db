941ad5c3a1aafc7f56226f0c7f44e2762db4373a
Juan Cuartas <equisoide@gmail.com>
2020-08-07 13:21:47 -0500

    Created PolicyCoverage table

---
+++ source/Gap.Insurance.Database/dbo/Tables/PolicyCoverage.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[PolicyCoverage]
+(
+	[PolicyCoverageId] INT NOT NULL,
+    [PolicyId] INT NOT NULL,
+    [CoverageId] INT NOT NULL,
+    [Percentage] DECIMAL(18,4) NOT NULL,
+	CONSTRAINT [PK_PolicyCoverage] PRIMARY KEY CLUSTERED ([PolicyCoverageId] ASC)
+);
