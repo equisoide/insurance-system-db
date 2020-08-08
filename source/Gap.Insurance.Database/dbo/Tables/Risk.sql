522bf2c5dc4ef9a835a47e150cd26cfeda465868
Juan Cuartas <equisoide@gmail.com>
2020-08-07 22:00:40 -0500

    Added MaxCoverage column to Risk table

--- source/Gap.Insurance.Database/dbo/Tables/Risk.sql
+++ source/Gap.Insurance.Database/dbo/Tables/Risk.sql
@@ -2,7 +2,9 @@
 (
 	[RiskId] INT NOT NULL,
 	[Description] NVARCHAR(50) NOT NULL,
+	[MaxCoverage] DECIMAL(18,2) NOT NULL,
 
 	CONSTRAINT [PK_Risk] PRIMARY KEY CLUSTERED ([RiskId] ASC),
-	CONSTRAINT [UK_Risk_Description] UNIQUE ([Description] ASC)
+	CONSTRAINT [UK_Risk_Description] UNIQUE ([Description] ASC),
+	CONSTRAINT [CK_Risk_MaxCoverage] CHECK  ([MaxCoverage] > 0 AND [MaxCoverage] <= 100)
 );
